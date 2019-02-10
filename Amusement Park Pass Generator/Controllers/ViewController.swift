ab9478f221b5a7c38b2b35397a6b06624e0842b8
bharathCTreehouse <bharathc6787@gmail.com>
2019-02-10 12:36:26 +0530

    Added comments.
    Added comments.

--- Amusement Park Pass Generator/Controllers/ViewController.swift
+++ Amusement Park Pass Generator/Controllers/ViewController.swift
@@ -46,40 +46,63 @@ class ViewController: UIViewController {
 //Pass generation
 extension ViewController {
     
+    /*Data for this method ideally should come from the UI, which shall be implemented in the second part of this project. For now, all the user information is hardcoded. */
+    
+    
     @IBAction func generatePassButtonTapped(_ sender: UIButton) {
+        
         
         var typeOfEntrant: EntrantType? = nil
         
-        //Reset the current pass and begin.
-        //entrantPass = nil
-
         
         switch sender.tag {
             
+            //Classic guest
             case 1: typeOfEntrant = EntrantType.guest(.classic)
             
+            
+            //VIP guest
             case 2: typeOfEntrant = EntrantType.guest(.vip)
             
+            
+            //Free child guest (The child has to be below 5 years of age)
             case 3:
                 let bDateFormatter: DateFormatter = DateFormatter()
                 bDateFormatter.dateFormat = "MMM d, yyyy"
+                
+                //Valid date but not his birthday (Uncomment this to set a valid date but not his birthdate)
+                //let bDate: Date? = bDateFormatter.date(from: "Feb 1, 2017")
+                
+                //Invalid date (Uncomment this to see if an appropriate error is shown during pass generation)
+                //let bDate: Date? = bDateFormatter.date(from: "Mar 20, 2010")
+                
+                //Today is his birthday.(Uncomment this and put today's date to see a birthday wish on swiping at a counter where he has access.)
                 let bDate: Date? = bDateFormatter.date(from: "Feb 10, 2017")
+
                 typeOfEntrant = EntrantType.guest(.freeChild(dateOfBirth: bDate!))
             
+            
+            //Employee-food
             case 4:
                 let fName: String = (self.missingInfoSwitch.isOn == true) ? "":"abc"
                 typeOfEntrant =
                     EntrantType.employee(.foodServices(PersonName(firstName:fName, lastName:"xxx"), Address(streetAddress:"India", city:"BLR", state:nil, zipCode:nil)))
             
+            
+            //Employee-ride services
             case 5:
                 let lName: String = (self.missingInfoSwitch.isOn == true) ? "$#&    ":"xxx"
                 typeOfEntrant = EntrantType.employee(.rideServices(PersonName(firstName:"ddd", lastName:lName), Address(streetAddress:"India", city:"BLR", state:nil, zipCode:nil)))
             
+            
+            //Employee-maintenance
             case 6:
                 let street: String = (self.missingInfoSwitch.isOn == true) ? "":"India"
                 typeOfEntrant = EntrantType.employee(.maintenance(PersonName(firstName:"ddd", lastName:"xxx"), Address(streetAddress:street, city:"BLR", state:nil, zipCode:nil)))
             
-             case 7:
+            
+            //Employee-manager
+            case 7:
                 let city: String = (self.missingInfoSwitch.isOn == true) ? "":"BLR"
                 typeOfEntrant = EntrantType.manager(PersonName(firstName:"ddd", lastName:"xxx"), Address(streetAddress:"India", city:city, state:nil, zipCode:nil))
             
@@ -155,7 +178,7 @@ extension ViewController {
                 
                 case 13:
                     let rideAccess: (Bool,Bool) = try kioskManager.swipeForRideWith(pass: entrantPass!)
-                    handleAccessToRide(.all, withPermsissionDetails: rideAccess)
+                    handleAccessToRide(withPermsissionDetails: rideAccess)
                 
                 case 14:
                     let foodDiscount: Int? = try kioskManager.swipeForFoodWith(pass: entrantPass!)
@@ -201,14 +224,14 @@ extension ViewController {
     }
     
     
-    func handleAccessToRide(_ ride: RideAccess, withPermsissionDetails details: (hasAccess: Bool, canSkipLines: Bool)) {
+    func handleAccessToRide(withPermsissionDetails details: (hasAccess: Bool, canSkipLines: Bool)) {
         
         if details.hasAccess == true {
             
             //Access granted.
-            var userText: String = "Access granted! \(ride.rideAccessString())"
+            var userText: String = "Access granted! \(RideAccess.all.rideAccessString())"
             if details.canSkipLines == true {
-                userText = userText + "\(RideAccess.skipRideLines.rideAccessString())"
+                userText = userText + ". \(RideAccess.skipRideLines.rideAccessString())"
             }
             self.accessStatusLabel.text = userText
             
